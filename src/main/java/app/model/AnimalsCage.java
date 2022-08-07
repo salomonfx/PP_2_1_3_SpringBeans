3e2e43c0ca19cc5cc5b20e5b86f2e258d64c6c61
salomonfx <99074676+salomonfx@users.noreply.github.com>
2022-08-07 20:04:39 +0300

    Initial commit

--- src/main/java/app/model/AnimalsCage.java
+++ src/main/java/app/model/AnimalsCage.java
@@ -1,13 +1,16 @@
 package app.model;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
 
 @Component
 public class AnimalsCage {
 
     @Autowired
+    @Qualifier("dog")
     private Animal animal;
+
 
     public void whatAnimalSay() {
         System.out.println("Say:");
@@ -15,5 +18,10 @@ public class AnimalsCage {
         System.out.println("At:");
         System.out.println(new Timer().getTime());
         System.out.println("________________________");
+    }
+    @Autowired
+    private Timer timer;
+    public Timer getTimer() {
+        return timer;
     }
 }
